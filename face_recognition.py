384985e268f3cea2e5ee471f894452db534ebe5a
kenchangh <guanhao3797@gmail.com>
2019-06-24 00:12:35 +0800

    added preprocessing test, refactored recognition code

--- face_recognition.py
+++ face_recognition.py
@@ -8,67 +8,87 @@ Created on Sun May 26 22:12:15 2019
 import cv2
 import pickle
 
+
 def nothing(x):
     pass
 
+
 def drawText(frame, name, x, y):
-    cv2.putText(frame, name, (x,y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-    
+    cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
+                1, (255, 255, 255), 2, cv2.LINE_AA)
+
+
 def drawRectangle(frame, rect):
-    cv2.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+    x, y, w, h = rect
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
 
 def createTrackbar():
     cv2.namedWindow("Frame")
     cv2.createTrackbar("Scale", "Frame", 0, 5, nothing)
     cv2.createTrackbar("Neighbours", "Frame", 5, 20, nothing)
 
-labels = {}
-with open("labels.pickle", 'rb') as f:
-    og_labels = pickle.load(f)
-    labels = {v:k for k,v in og_labels.items()}
-    
-cap = cv2.VideoCapture(0)
 
-face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-recognizer = cv2.face.EigenFaceRecognizer_create()
-recognizer.read("trainner.yml2")
+class FaceRecognition:
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier(
+            "haarcascade_frontalface_default.xml")
+        self.recognizer = cv2.face.EigenFaceRecognizer_create()
+        self.recognizer.read("trainner.yml2")
+        self.labels = {}
+        self.read_labels()
 
-createTrackbar()
+    def read_labels(self):
+        with open("labels.pickle", 'rb') as f:
+            og_labels = pickle.load(f)
+            labels = {v: k for k, v in og_labels.items()}
+            self.labels.update(labels)
 
-while True:
-    ret, frame = cap.read()  
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    scale = cv2.getTrackbarPos("Scale", "Frame")
-    neighbours = cv2.getTrackbarPos("Neighbours", "Frame")
-    
-    faces = face_cascade.detectMultiScale(gray, (scale+11)/10, neighbours)
-    
-    for rect in faces:
-        (x, y, w, h) = rect
-        roi = gray[y:y+h, x:x+w]
+    def start_interactive_session(self):
+        cap = cv2.VideoCapture(0)
+        createTrackbar()
+
+        while True:
+            ret, frame = cap.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            scale = cv2.getTrackbarPos("Scale", "Frame")
+            neighbours = cv2.getTrackbarPos("Neighbours", "Frame")
+
+            faces = self.face_cascade.detectMultiScale(
+                gray, (scale+11)/10, neighbours)
+
+            for rect in faces:
+                (x, y, w, h) = rect
+                roi = gray[y:y+h, x:x+w]
+                id_, conf = self.recognizer.predict(
+                    cv2.resize(roi, (300, 300)))
+
+                if conf <= 30000:
+                    print(id_)
+                    print(self.labels[id_])
+                    name = self.labels[id_]
+                else:
+                    name = "unknown"
+
+                drawText(frame, name, x, y)
+                drawRectangle(frame, rect)
+
+            image = cv2.resize(frame, (750, 500))
+            cv2.imshow("Frame", image)
+
+            key = cv2.waitKey(1)
+
+            if key == 27:
+                break
+
         id_, conf = recognizer.predict(cv2.resize(roi, (300, 300)))
-         
-        if conf <=30000:
-            print(id_)
-            print(labels[id_])           
-            name = labels[id_]         
-        else:
-            name = "unknown"
-        
-        drawText(frame, name, x, y)
-        drawRectangle(frame, rect)
+        print(conf)
 
-    image = cv2.resize(frame,(750, 500))
-    cv2.imshow("Frame", image)
-    
-    key = cv2.waitKey(1)
-    
-    if key ==27:
-        break
-    
-id_, conf = recognizer.predict(cv2.resize(roi, (300, 300)))
-print(conf) 
-  
-cap.release()
-cv2.destroyAllWindows()
+        cap.release()
+        cv2.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    recognition = FaceRecognition()
+    recognition.start_interactive_session()
--- preprocessing.py
+++ preprocessing.py
@@ -1,25 +1,48 @@
+import os
+import cv2
 import numpy as np
+from scipy import ndimage
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+images_dir = os.path.join(ROOT_DIR, 'images')
 
 
 class TanTriggsPreprocessing:
-    def __init__(self, alpha = 0.1, tau = 10.0, gamma = 0.2, sigma0 = 1.0, sigma1 = 2.0):
+    def __init__(self, alpha=0.1, tau=10.0, gamma=0.2, sigma0=1.0, sigma1=2.0):
         self._alpha = float(alpha)
         self._tau = float(tau)
         self._gamma = float(gamma)
         self._sigma0 = float(sigma0)
         self._sigma1 = float(sigma1)
-    
-    def compute(self,X,y):
+
+    def compute(self, X):
         Xp = []
         for xi in X:
             Xp.append(self.extract(xi))
         return Xp
 
-    def extract(self,X):
+    def extract(self, X):
         X = np.array(X, dtype=np.float32)
-        X = np.power(X,self._gamma)
-        X = np.asarray(ndimage.gaussian_filter(X,self._sigma1) - ndimage.gaussian_filter(X,self._sigma0))
-        X = X / np.power(np.mean(np.power(np.abs(X),self._alpha)), 1.0/self._alpha)
-        X = X / np.power(np.mean(np.power(np.minimum(np.abs(X),self._tau),self._alpha)), 1.0/self._alpha)
+        X = np.power(X, self._gamma)
+        X = np.asarray(ndimage.gaussian_filter(X, self._sigma1) -
+                       ndimage.gaussian_filter(X, self._sigma0))
+        X = X / \
+            np.power(np.mean(np.power(np.abs(X), self._alpha)), 1.0/self._alpha)
+        X = X / np.power(np.mean(np.power(np.minimum(np.abs(X),
+                                                     self._tau), self._alpha)), 1.0/self._alpha)
         X = self._tau*np.tanh(X/self._tau)
         return X
+
+
+def test_preprocessing(img_path):
+    img = cv2.imread(img_path)
+    preprocessing_algo = TanTriggsPreprocessing()
+    processed = np.array(preprocessing_algo.compute(img))
+
+    cv2.imshow('show processed', processed)
+    cv2.waitKey(10000)
+
+
+if __name__ == '__main__':
+    image_path = os.path.join(images_dir, 'emilia-clarke', '1.jpg')
+    test_preprocessing(image_path)
