0e2606c34603a660cd410c970acbdbe5c7d7cd6c
Yap Jia Yung <yapjiayung@gmail.com>
2019-06-25 23:44:29 +0800

    Added rotated detection

--- face_recognition.py
+++ face_recognition.py
@@ -8,8 +8,7 @@ Created on Sun May 26 22:12:15 2019
 import cv2
 import pickle
 from preprocessing import TanTriggsPreprocessing
-from collections import Counter
-
+from math import sin, cos, radians
 
 def nothing(x):
     pass
@@ -27,11 +26,28 @@ def drawRectangle(frame, rect):
 
 def createTrackbar():
     cv2.namedWindow("Frame")
-    cv2.createTrackbar("Scale", "Frame", 0, 5, nothing)
+    cv2.createTrackbar("Scale", "Frame", 5, 5, nothing)
     cv2.createTrackbar("Neighbours", "Frame", 5, 20, nothing)
+
+def rotate_image(image, angle):
+    if angle == 0: return image
+    height, width = image.shape[:2]
+    rot_mat = cv2.getRotationMatrix2D((width/2, height/2), angle, 0.9)
+    result = cv2.warpAffine(image, rot_mat, (width, height), flags=cv2.INTER_LINEAR)
+    return result
+
+def rotate_point(pos, img, angle):
+    if angle == 0: return pos
+    x = pos[0] - img.shape[1]*0.4
+    y = pos[1] - img.shape[0]*0.4
+    newx = x*cos(radians(angle)) + y*sin(radians(angle)) + img.shape[1]*0.4
+    newy = -x*sin(radians(angle)) + y*cos(radians(angle)) + img.shape[0]*0.4
+    return int(newx), int(newy), pos[2], pos[3]
 
 
 class FaceRecognition:
+    
+  
     def __init__(self):
         self.face_cascade = cv2.CascadeClassifier(
             "haarcascade_frontalface_default.xml")
@@ -42,7 +58,7 @@ class FaceRecognition:
         self.count = 0
         self.people = []
         self.temporary = []
-        self.toBeRemove = []
+        self.toBeRemove = []       
         self.Removed = []
 
     def read_labels(self):
@@ -58,41 +74,46 @@ class FaceRecognition:
 
         while True:
             ret, frame = cap.read()
+            
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
             scale = cv2.getTrackbarPos("Scale", "Frame")
             neighbours = cv2.getTrackbarPos("Neighbours", "Frame")
-
-            faces = self.face_cascade.detectMultiScale(
-                gray, (scale+11)/10, neighbours)
-
-            for rect in faces:
-                (x, y, w, h) = rect
-                roi = gray[y:y+h, x:x+w]
-                # roi = cv2.equalizeHist(roi)
-                clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-                roi = clahe.apply(roi)
-                # cv2.imshow('show', roi)
-                # cv2.waitKey(1000)
-                id_, conf = self.recognizer.predict(
-                    cv2.resize(roi, (300, 300)))
-
-                if conf <= 55:
-                   
-                    print(id_)
-                    print(self.labels[id_])
-                    name = self.labels[id_]
-                    print("%s %s" %(name, conf))
-                    self.temporary.append(name)
-                else:
-                    name = "unknown"
-                    print("Unknown", conf)
+            
+            for angle in [0, -25, 25]:
+                rimg = rotate_image(gray, angle)
+                faces = self.face_cascade.detectMultiScale(rimg, (scale+11)/10, neighbours)
+            
+                for face in faces:
+                    rotatedFace = [rotate_point(face, gray, -angle)]
+                           
+                    for rect in rotatedFace:
+                        (x, y, w, h) = rect
+                        roi = gray[y:y+h, x:x+w]
+                        # roi = cv2.equalizeHist(roi)
+                        #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+                        #roi = clahe.apply(roi)
+                        # cv2.imshow('show', roi)
+                        # cv2.waitKey(1000)
+                        id_, conf = self.recognizer.predict(
+                            cv2.resize(roi, (300, 300)))
+        
+                        if conf <= 35:
+                           
+                            print(id_)
+                            print(self.labels[id_])
+                            name = self.labels[id_]
+                            print("%s %s" %(name, conf))
+                            self.temporary.append(name)
+                        else:
+                            name = "unknown"
+                            print("Unknown", conf)
+                            
+                        drawText(frame, name, x, y)
+                        drawRectangle(frame, rect)
                     
-                drawText(frame, name, x, y)
-                drawRectangle(frame, rect)
-                    
-            image = cv2.resize(frame, (750, 500))
-            cv2.imshow("Frame", image)
+                image = cv2.resize(frame, (750, 500))
+                cv2.imshow("Frame", image)
 
             key = cv2.waitKey(1)
 
@@ -101,8 +122,7 @@ class FaceRecognition:
 
         id_, conf = self.recognizer.predict(cv2.resize(roi, (300, 300)))
         print(conf)
-        
-        
+                
 #        for name in self.people:
 #            if (self.people.count(name)/len(self.people)) < 0.5:
 #                self.toBeRemove.append(name)
@@ -117,7 +137,6 @@ class FaceRecognition:
 
         cap.release()
         cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     recognition = FaceRecognition()
