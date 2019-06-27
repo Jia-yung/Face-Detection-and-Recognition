d72c983645554e459172b671fa808023638ebfaa
Yap Jia Yung <yapjiayung@gmail.com>
2019-06-28 03:22:54 +0800

    Final version

--- face_recognition.py
+++ face_recognition.py
@@ -7,35 +7,35 @@ Created on Sun May 26 22:12:15 2019
 """
 import cv2
 import pickle
-from preprocessing import TanTriggsPreprocessing
 from math import sin, cos, radians
 
 def nothing(x):
     pass
 
-
+#function to write the name of the detected faces.
 def drawText(frame, name, x, y):
     cv2.putText(frame, name, (x, y), cv2.FONT_HERSHEY_SIMPLEX,
                 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-
+#function to draw a green bounding rectangle of the detected faces.
 def drawRectangle(frame, rect):
     x, y, w, h = rect
     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-
+#trackbar used for adjusting the scale factor and min neighors for face detection using Haar Cascade
 def createTrackbar():
     cv2.namedWindow("Frame")
-    cv2.createTrackbar("Scale", "Frame", 5, 5, nothing)
+    cv2.createTrackbar("Scale", "Frame", 5, 20, nothing)
     cv2.createTrackbar("Neighbours", "Frame", 5, 20, nothing)
 
+#rotates the image array by  angle degree
 def rotate_image(image, angle):
     if angle == 0: return image
     height, width = image.shape[:2]
     rot_mat = cv2.getRotationMatrix2D((width/2, height/2), angle, 0.9)
     result = cv2.warpAffine(image, rot_mat, (width, height), flags=cv2.INTER_LINEAR)
     return result
-
+#
 def rotate_point(pos, img, angle):
     if angle == 0: return pos
     x = pos[0] - img.shape[1]*0.4
@@ -44,61 +44,125 @@ def rotate_point(pos, img, angle):
     newy = -x*sin(radians(angle)) + y*cos(radians(angle)) + img.shape[0]*0.4
     return int(newx), int(newy), pos[2], pos[3]
 
-
 class FaceRecognition:
-    
-  
+     
     def __init__(self):
+        
+        #creates the cascade classification from the haarcascade_frontalface classification file
         self.face_cascade = cv2.CascadeClassifier(
             "haarcascade_frontalface_default.xml")
         self.recognizer = cv2.face.LBPHFaceRecognizer_create()
+        
+        #read the file containing all the trained datasets of individuals
         self.recognizer.read("trainner.yml2")
         self.labels = {}
         self.read_labels()
         self.count = 0
         self.people = []
         self.temporary = []
-        self.toBeRemove = []       
-        self.Removed = []
-
+    
     def read_labels(self):
         with open("labels.pickle", 'rb') as f:
             og_labels = pickle.load(f)
             labels = {v: k for k, v in og_labels.items()}
             self.labels.update(labels)
 
+    def start_interactive_session_images(self):        
+        
+        #read the input image use for face detection and recognition
+        img = cv2.imread("brightClass.jpg")
+        
+        #apply gaussian blur to remove noises
+        blurredImg = cv2.GaussianBlur(img, (3, 3), 0)
+        
+        #convert image from rgb to gray scale
+        gray = cv2.cvtColor(blurredImg, cv2.COLOR_BGR2GRAY)
+        
+        for angle in [0, -25, 25]:
+                                                            
+            rimg = rotate_image(gray, angle)
+            
+            #store the detected face in faces array
+            faces = self.face_cascade.detectMultiScale(rimg, 1.5, 5)
+            
+            for face in faces:
+                rotatedFace = [rotate_point(face, gray, -angle)]
+                       
+                for index, rect in enumerate(rotatedFace):
+                    #get the x,y position and width, height of the detected face
+                    (x, y, w, h) = rect
+                    
+                    #extract the region of interest where the face is detected using x, y, w, h values
+                    roi = gray[y:y+h, x:x+w]
+                  
+                    #use LBPH to predict face within the region of interest
+                    #returns a confidence level of how similar the input face to the faces in the trained dataset
+                    #returns an ID of the trained individual
+                    id_, conf = self.recognizer.predict(roi)
+                    
+                    #set a threshold for the distance between the input images and trained images in database
+                    #if the difference in terms of distance is less than the threshold
+                    #assign the name of the person to the one that have the closest match in the trained datatset
+                    #0 indicates a perfect match
+                    if conf <= 100:
+                        
+                        print(id_)
+                        print(self.labels[id_])
+                        name = self.labels[id_]
+                        print("%s %s" %(name, conf))
+                        
+                        #assign the name of the recognised individual to an array to keep track of the people present in class
+                        self.temporary.append(name)
+                    else:
+                        #assign unknown to name if the difference between the compared images are above threhold level
+                        name = "unknown"
+                        print("Unknown", conf)
+                     
+                    #write the name of the individual at the coordinate where the face is detected
+                    drawText(img, name, x, y)
+                    
+                    #draw a bounding rectangle around the detected face
+                    drawRectangle(img, rect)
+                
+            #display the output image
+            image = cv2.resize(img, (1024, 640))
+            cv2.imshow("Frame", image)
+            key = cv2.waitKey(1)
+            
+            if key == 27:
+                break            
+    
+    #function for video capture
+    #face detection and recognition procedure is same as start_interactive_session_images
     def start_interactive_session(self):
         cap = cv2.VideoCapture(0)
-        createTrackbar()
-        #preprocessing_algo = TanTriggsPreprocessing()
-
+        createTrackbar()        
+        
         while True:
             ret, frame = cap.read()
             
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+            blurredImg = cv2.GaussianBlur(frame, (3, 3), 0)
+            
+            gray = cv2.cvtColor(blurredImg, cv2.COLOR_BGR2GRAY)
+            
             scale = cv2.getTrackbarPos("Scale", "Frame")
             neighbours = cv2.getTrackbarPos("Neighbours", "Frame")
             
             for angle in [0, -25, 25]:
                 rimg = rotate_image(gray, angle)
                 faces = self.face_cascade.detectMultiScale(rimg, (scale+11)/10, neighbours)
+                
             
                 for face in faces:
                     rotatedFace = [rotate_point(face, gray, -angle)]
                            
-                    for rect in rotatedFace:
+                    for index, rect in enumerate(rotatedFace):
                         (x, y, w, h) = rect
                         roi = gray[y:y+h, x:x+w]
-                        # roi = cv2.equalizeHist(roi)
-                        #clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-                        #roi = clahe.apply(roi)
-                        # cv2.imshow('show', roi)
-                        # cv2.waitKey(1000)
-                        id_, conf = self.recognizer.predict(
-                            cv2.resize(roi, (300, 300)))
-        
-                        if conf <= 35:
+                      
+                        id_, conf = self.recognizer.predict(roi)
+                        
+                        if conf <= 100:
                            
                             print(id_)
                             print(self.labels[id_])
@@ -108,7 +172,7 @@ class FaceRecognition:
                         else:
                             name = "unknown"
                             print("Unknown", conf)
-                            
+                                                    
                         drawText(frame, name, x, y)
                         drawRectangle(frame, rect)
                     
@@ -119,17 +183,7 @@ class FaceRecognition:
 
             if key == 27:
                 break
-
-        id_, conf = self.recognizer.predict(cv2.resize(roi, (300, 300)))
-        print(conf)
-                
-#        for name in self.people:
-#            if (self.people.count(name)/len(self.people)) < 0.5:
-#                self.toBeRemove.append(name)
-#             
-#                
-#        self.Removed = [x for x in self.people if x not in self.toBeRemove]
-        
+                        
         for name in self.temporary:
             if name not in self.people:
                 self.people.append(name)
@@ -138,6 +192,8 @@ class FaceRecognition:
         cap.release()
         cv2.destroyAllWindows()
 
+#main program to start the execution of code
 if __name__ == '__main__':
     recognition = FaceRecognition()
-    recognition.start_interactive_session()
+    #recognition.start_interactive_session()
+    recognition.start_interactive_session_images()
--- faces_train.py
+++ faces_train.py
@@ -15,7 +15,10 @@ import pickle
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
 
+#creates the cascade classification from the haarcascade_frontalface classification file
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+#creates the facial recogniser using opencv LBPH 
 recognizer = cv2.face.LBPHFaceRecognizer_create()
 
 current_id = 0
@@ -23,8 +26,11 @@ label_ids = {}
 y_labels = []
 x_train = []
 
+#loop through the files to train the recogniser 
 for root, dirs, files in os.walk(image_dir):
     for file in files:
+        
+        #only train with files that ends with png or jpg format
         if file.endswith("png") or file.endswith("jpg") or file.endswith("JPG"):
             path = os.path.join(root, file)
             label = os.path.basename(os.path.dirname(
@@ -38,36 +44,36 @@ for root, dirs, files in os.walk(image_dir):
             id_ = label_ids[label]
             print(label_ids)
 
+            #display the training images
             cv2.imshow("Training on image...", cv2.resize(
                 cv2.imread(path), (600, 500)))
             cv2.waitKey(100)
 
             pil_image = Image.open(path).convert("L")
-            size = (550, 550)
-            final_image = pil_image.resize(size, Image.ANTIALIAS)
-            image_array = np.array(final_image, dtype="uint8")
-
+            #size = (550, 550)
+            #final_image = pil_image.resize(size, Image.ANTIALIAS)
+            image_array = np.array(pil_image, dtype="uint8")
+            
+            #store the detected face in faces array using HaarCascade
             faces = face_cascade.detectMultiScale(
-                image_array, scaleFactor=1.4, minNeighbors=3)
+                image_array, scaleFactor=1.3, minNeighbors=5)
 
+            #get the x,y position and width, height of the detected face
             for(x, y, w, h) in faces:
-                feature = ()
+                #feature = ()
                 roi = image_array[y:y+h, x:x+w]
-                clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-                equalized = clahe.apply(roi)
-                # res = np.hstack((roi, equalized))
-                # cv2.imshow("Training on image...", cv2.resize(
-                #     res, (600, 500)))
-                # cv2.waitKey(1000)
-                x_train.append(cv2.resize(equalized, (300, 300)))
+                #append the faces for training into x_train and their respective lables into y_labels
+                x_train.append(roi)
                 y_labels.append(id_)
 
-# print(y_labels)
-
+#using pickle to save the labels ID
 with open("labels.pickle", 'wb') as f:
     pickle.dump(label_ids, f)
 
+#use the train function to extract features in facial images and converts them to histogram
 recognizer.train(x_train, np.array(y_labels))
+
+#save the feature values into a yml file for comparison with input images in recognition process
 recognizer.save("trainner.yml2")
 
 print("Training Complete")
